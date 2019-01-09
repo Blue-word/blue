252a2a22e5079a7f07185a36e8e9ec63d5fc6721
Blue <1052446882@qq.com>
2019-01-09 21:12:38 +0800

    去除runtime

--- runtime/cache/0a/71ec94f06b4c8f198df7e3826bcbf7.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:3:{i:0;s:125:"Pacificocean@course_list,Pacificocean@course_info,Pacificocean@course_handle,Pacificocean@course_view,Pacificocean@apply_list";i:1;s:43:"Pacificocean@new_list,Pacificocean@new_info";i:2;s:59:"Pacificocean@class_type_list,Pacificocean@class_type_handle";}
